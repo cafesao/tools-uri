834bedd30120026a338069fbe29533cb69553717
cafesao <my@cafesao.net>
2025-06-28 18:36:13 -0300

    feat: add rollup-plugin-visualizer for build analysis and update configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import path from "node:path";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react-swc";
+import { visualizer } from "rollup-plugin-visualizer";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
@@ -9,6 +10,29 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id: string) {
+          if (id.includes("node_modules")) {
+            return id
+              .toString()
+              .split("node_modules/")[1]
+              .split("/")[0]
+              .toString();
+          }
+        },
+      },
+      plugins: [
+        visualizer({
+          open: false,
+          filename: "dist/stats.html",
+          gzipSize: true,
+          brotliSize: true,
+        }),
+      ],
     },
   },
   server: {
